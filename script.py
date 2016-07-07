d69a676d2426660e33ea15bcf4d0d9e9191363d8
Sami Sabik <sabik.sami@gmail.com>
2016-07-07 20:52:03 +1000

    up

--- script.py
+++ script.py
@@ -4,6 +4,17 @@ import Image,sys
 from PIL import Image
 import pytesseract
 from PIL import ImageFilter
+import Image,sys
+from PIL import Image
+from PIL import ImageEnhance
+import pytesseract
+from PIL import ImageFilter
+
+left = 900
+top = 850
+width = 1000
+height = 200
+box = (left, top, left+width, top+height)
 
 camera = picamera.PiCamera()
 camera.resolution = (2592, 1944)
@@ -18,7 +29,9 @@ while True:
         time.sleep(10)
         camera.capture('output/image.jpg')
         im = Image.open("output/image.jpg")
-        #im = im.filter(ImageFilter.BLUR)
+        im = im.crop(box)
+        enhancer = ImageEnhance.Contrast(imc)
+        im = enhancer.enhance(5)
         text = pytesseract.image_to_string(im)
         if text != "":
                 text_file = open("output/text.txt", "w")
