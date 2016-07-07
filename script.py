ab23cfc3960119ddec30479600464ab57f38e342
Sami Sabik <sabik.sami@gmail.com>
2016-07-07 23:27:31 +1000

    clean

--- script.py
+++ script.py
@@ -1,17 +1,10 @@
 #!/usr/bin/env python
-import picamera,os,sys,time
-import Image,sys
-from PIL import Image
+import picamera,os,sys,time,Image
+from PIL import Image,ImageFilter,ImageEnhance
 import pytesseract
-from PIL import ImageFilter
-import Image,sys
-from PIL import Image
-from PIL import ImageEnhance
-import pytesseract
-from PIL import ImageFilter
 
-left = 900
-top = 800
+left = 1050
+top = 1000
 width = 1000
 height = 200
 box = (left, top, left+width, top+height)
@@ -26,14 +19,14 @@ text_file.close()
 os.system('lp -d Star-TUP900-STR-T-U001 -o PresenterAction=4NoLoopNoHoldEject output/text.txt')
 
 while True:
-        time.sleep(10)
+        time.sleep(5)
         camera.capture('output/image.jpg')
         im = Image.open("output/image.jpg")
         im = im.crop(box)
-        enhancer = ImageEnhance.Contrast(im)
-        im = enhancer.enhance(3)
+        im.save("output/edit.jpg")
         text = pytesseract.image_to_string(im)
         if text != "":
+                print text
                 text_file = open("output/text.txt", "w")
                 text_file.write(text)
                 text_file.close()
